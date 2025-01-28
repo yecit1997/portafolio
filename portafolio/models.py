1695936510334a12ca475b7200a05b073135a44c
yecit1997 <ttorresyecit@gmail.com>
2025-01-27 20:32:36 -0500

    Estilizacion del sitio

--- portafolio/models.py
+++ portafolio/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tecnologias(models.Model):
+    nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='tecnologias')
+
+    class Meta:
+        verbose_name = 'Tecnologia'
+        verbose_name_plural = 'Tecnologias'
+        
+    def __str__(self) -> str:
+        return self.nombre
+
+
+
+class Proyectos(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='proyectos')
+    tecnologias = models.ManyToManyField(Tecnologias, related_name='proyectos')
+
+    class Meta:
+        verbose_name = 'Proyecto'
+        verbose_name_plural = 'Proyectos'
+
+    def __str__(self):
+        return self.nombre
+    
+
+
+
+
+
