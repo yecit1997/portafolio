66cfc2c0ecbbc44bcbd8e43a18361cce1369b0f9
yecit1997 <ttorresyecit@gmail.com>
2025-04-20 19:21:13 -0500

    Configuramos la base de datos

--- core/settings.py
+++ core/settings.py
@@ -93,12 +93,10 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        # conn_max_age=600,
-        # ssl_require=True,
-    )
-}
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    }
+
+print("DATABASE_URL", os.getenv("DATABASE_URL"))
 
 
 # Password validation
