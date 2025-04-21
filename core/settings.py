30f704e972ddddd6acf99917f5dbb935d34602d0
yecit1997 <ttorresyecit@gmail.com>
2025-04-20 21:05:10 -0500

    Modificamos el paquete psycopg2 por psycopg2-binary

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,9 @@ STATICFILES_DIRS = [
 ]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
