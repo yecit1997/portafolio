b0e1c45c21d0f1fb05a7666c370b2cce73986efe
yecit1997 <ttorresyecit@gmail.com>
2025-04-20 20:55:24 -0500

    Modificamos el paquete psycopg2 por psycopg2-binary

--- core/settings.py
+++ core/settings.py
@@ -91,10 +91,17 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
-    }
+# DATABASES = {
+#     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+#     }
 
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
