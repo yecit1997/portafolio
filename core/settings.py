1cf56e1e888d486673c12fe2daf049ec5a4477fd
yecit1997 <ttorresyecit@gmail.com>
2025-09-13 12:15:38 -0500

    Configuarcion de CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -167,5 +167,5 @@ ALLOWED_HOSTS.append("https://yecit1997.pythonanywhere.com/")
 CSRF_TRUSTED_ORIGINS = [
     "https://web-production-0f15b.up.railway.app",
     "http://*",
-    "yecit1997.pythonanywhere.com/"
+    "http://yecit1997.pythonanywhere.com/"
 ]
