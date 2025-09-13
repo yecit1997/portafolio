8856edaf149a429ecd9f15b492c53428516e6b5a
yecit1997 <ttorresyecit@gmail.com>
2025-09-12 20:54:59 -0500

    Configurar wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+
+import os
+import sys
+
+path = '/home/yecit1997/portafolio' # Ruta a tu proyecto de Django
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings' # Reemplaza 'tu_proyecto' con el nombre real de tu proyecto
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
